e4bc0182af843dcce3e39ba6e356e5daa6fa3541
primary157 <victorgvbh@gmail.com>
2015-03-01 20:30:20 -0300

    revolta

--- sitetest/urls.py
+++ sitetest/urls.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
+<<<<<<< HEAD
     url(r'^$', 'sitetest.views.home', name='home'),
+    url(r'^blog/', include('blog.urls')),
+=======
+    # url(r'^$', 'sitetest.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+>>>>>>> parent of 9be75e4... uncomment for testing
 
     url(r'^admin/', include(admin.site.urls)),
 )
