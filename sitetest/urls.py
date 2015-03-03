1e3d2dc2d1e0f445f07d9ee707bc0143a243a41c
primary157 <victorgvbh@gmail.com>
2015-03-03 02:32:05 -0300

    Revert "mudança maluca"
    This reverts commit 32a608069c14544d765ea193795006b601ab09d0, reversing
    changes made to 1678c578e941940935afa2e068e8869810339fd0.

--- sitetest/urls.py
+++ sitetest/urls.py
@@ -5,17 +5,8 @@ from coisa import views
 urlpatterns = patterns('',
     url(r'^testt/', views.index, name='index'),
     # Examples:
-<<<<<<< HEAD
     url(r'^$', 'sitetest.views.home', name='home'),
-<<<<<<< HEAD
     #url(r'^blog/', include('blog.urls')),
-=======
-    url(r'^blog/', include('blog.urls')),
-=======
-    # url(r'^$', 'sitetest.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
->>>>>>> parent of 9be75e4... uncomment for testing
->>>>>>> master
 
     url(r'^admin/', include(admin.site.urls)),
 )
