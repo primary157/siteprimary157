1678c578e941940935afa2e068e8869810339fd0
primary157 <victorgvbh@gmail.com>
2015-03-03 02:06:43 -0300

    criaçao de um novo app com um novo url que retorna uma mensagem.

--- sitetest/urls.py
+++ sitetest/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from coisa import views
 
 urlpatterns = patterns('',
+    url(r'^testt/', views.index, name='index'),
     # Examples:
     url(r'^$', 'sitetest.views.home', name='home'),
-    url(r'^blog/', include('blog.urls')),
+    #url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
