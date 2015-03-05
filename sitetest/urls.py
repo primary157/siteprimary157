fdd4e7f3547a5363792e5ebac264f3c0792bd955
primary157 <victorgvbh@gmail.com>
2015-03-04 23:41:27 -0300

    Inicia Minha Jornada Pela Documentacao oficial!

--- sitetest/urls.py
+++ sitetest/urls.py
@@ -4,8 +4,9 @@ from django.conf import settings
 import coisa.views
 
 urlpatterns = patterns('',
-	url(r'^$', include('inicial.urls')),
-	url(r'^about/', coisa.views.index, name='about'),
+    url(r'^$', include('inicial.urls')),
+    url(r'^poll/', include('poll.urls')),
+    url(r'^about/', coisa.views.index, name='about'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^testt/', include('coisa.urls')),
 )
