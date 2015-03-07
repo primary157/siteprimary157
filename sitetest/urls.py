faa61f86d0713338b1dac265d3239de8ccf08744
primary157 <victorgvbh@gmail.com>
2015-03-07 01:31:57 -0300

    Consertado!

--- sitetest/urls.py
+++ sitetest/urls.py
@@ -1,18 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf import settings
 import coisa.views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', include('inicial.urls')),
     url(r'^poll/', include('poll.urls', namespace="poll")),
-    url(r'^about/', coisa.views.index, name='about'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^about/', coisa.views.index, name='about'),
     url(r'^testt/', include('coisa.urls', namespace="coisa")),
-)
-if settings.DEBUG:
-    urlpatterns += patterns(
-        'django.views.static',
-        (r'^media/(?P<path>.*)',
-        'serve',
-        {'document_root': settings.MEDIA_ROOT}), )
+]
