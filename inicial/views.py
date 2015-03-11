1787b2927db364eef94069a9ef7e5bc17b73dd32
primary157 <victorgvbh@gmail.com>
2015-03-10 23:35:47 -0300

    Adicionando pasta poll de Templates, atualizando pycache e settings

--- inicial/views.py
+++ inicial/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+
+
 def algo(request):
-	context_dict = {'boldmessage': "I am bold font from the context"}
-	return render(request,'loucura.html',context_dict)
-# Create your views here.
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, 'loucura.html', context_dict)
--- sitetest/settings.py
+++ sitetest/settings.py
@@ -50,22 +50,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 ROOT_URLCONF = 'sitetest.urls'
 
 WSGI_APPLICATION = 'sitetest.wsgi.application'
